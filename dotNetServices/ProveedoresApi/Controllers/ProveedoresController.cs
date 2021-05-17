f31f2c4d573e4447c87110ceb570af8c317f29b8
eduard.torres <eduard.torres10@hotmail.com>
2021-05-16 19:41:05 -0500

    Proveedores

---
+++ dotNetServices/ProveedoresApi/Controllers/ProveedoresController.cs
@@ -0,0 +1,43 @@
+﻿using ProveedoresCore.DTO;
+using ProveedoresCore.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProveedoresApi.Controllers
+{
+    [Route("fabricante")]
+    [ApiController]
+    public class ProveedoresController : ControllerBase
+    {
+        IProveedoresServices iProveedoresServices;
+        public ProveedoresController(IProveedoresServices _iProveedoresServices)
+        {
+            iProveedoresServices = _iProveedoresServices;
+        }
+
+        [HttpGet("listado/obtener")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ProveedorDTO>> ListarProveedores()
+        {
+            return Ok(await iProveedoresServices.ListarProveedores());
+        }
+
+        [HttpGet("productos/obtener/{filtro}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> BuscarProductosProveedores([FromRoute] string filtro)
+        {
+            return Ok(await iProveedoresServices.BuscarProductosProveedores(filtro));
+        }
+
+        [HttpGet("inventario/consultar")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<ProveedorDTO>> ConsultarInventario(IList<ProductoDTO> productos)
+        {
+            return Ok(await iProveedoresServices.ConsultarInventario(productos));
+        }
+    }
+}
