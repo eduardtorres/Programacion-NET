d3c8a8fade4bf751059f674aec69d4f8ac7ec088
c@rlos's mac <carlosarangob@hotmail.com>
2021-05-26 23:23:18 -0500

    mock de proveedor soap

---
+++ Mocks/proveedores/WcfServiceProveedor2/WcfServiceProveedor2/IService1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceProveedor2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        List<ProductoBE> GetDataUsingDataContract(string filtro);
+
+        [OperationContract]
+        OrdenResponse PonerOrdenExterna(OrdenRequest request);
+
+        // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class OrdenRequest
+    {
+        [DataMember]
+        public string codigoProducto { get; set; }
+        [DataMember]
+        public string cantidadPedida { get; set; }
+        [DataMember]
+        public string codigoTercero { get; set; }
+    }
+
+    [DataContract]
+    public class OrdenResponse
+    {
+        [DataMember]
+        public string numeroOrden { get; set; }
+        [DataMember]
+        public string statusOrden { get; set; }
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class ProductoBE
+    {
+
+        [DataMember]
+        public int id
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string codigo
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string nombre
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string linea
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public string descripcion
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double precioUnitario
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int unidadesDisponibles
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Mocks/proveedores/WcfServiceProveedor2/WcfServiceProveedor2/Service1.svc.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceProveedor2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public List<ProductoBE> GetDataUsingDataContract(string filtro)
+        {
+            List<ProductoBE> productos = new List<ProductoBE>();
+
+            productos.Add(new ProductoBE()
+            {
+                codigo = "P001",
+                linea = "Juegos PS5",
+                id = 2001,
+                descripcion = "FPS",
+                nombre = "Call of duty Cold War",
+                precioUnitario = 240000,
+                unidadesDisponibles = 10
+            });
+
+            productos.Add(new ProductoBE()
+            {
+                codigo = "P002",
+                linea = "Juegos PS5",
+                id = 2002,
+                descripcion = "FPS",
+                nombre = "Call of duty WWII",
+                precioUnitario = 140000,
+                unidadesDisponibles = 1
+            });
+
+            productos.Add(new ProductoBE()
+            {
+                codigo = "P003",
+                linea = "Juegos PS5",
+                id = 2003,
+                descripcion = "Survival horror",
+                nombre = "Resident evil 8 the Village",
+                precioUnitario = 200000,
+                unidadesDisponibles = 0
+
+            });
+
+            productos.Add(new ProductoBE()
+            {
+                codigo = "P004",
+                linea = "Juegos PS5",
+                id = 2004,
+                descripcion = "Souls",
+                nombre = "Demon Souls",
+                precioUnitario = 350000,
+                unidadesDisponibles = 25
+
+            });
+
+            return productos;
+        }
+
+        public OrdenResponse PonerOrdenExterna(OrdenRequest request)
+        {
+
+            return new OrdenResponse()
+            {
+                numeroOrden = (new Random(DateTime.Now.Second).Next()).ToString(),
+                statusOrden = "ACEPTADA"
+            };
+
+        }
+
+    }
+}
